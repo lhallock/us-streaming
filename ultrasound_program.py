8f03702ea0748224eade9210207c07aabc931d23
Bhavna Sud <bhavnasmile1@gmail.com>
2021-05-16 21:38:40 -0700

    works with latest amg_emg_force_control code

--- ultrasound_program.py
+++ ultrasound_program.py
@@ -3,17 +3,17 @@ from socketpython import SocketPython
 import sys
 sys.path.append('../')
 
-from amg_emg_force_control import *
-from amg_emg_force_control.grapher_game.game.main import Graphing
-FILENAME= "../amg_emg_force_control/grapher_game/test_scripts/ultrasound_grapher.txt"
+from amg_emg_force_control2 import *
+from amg_emg_force_control2.grapher_game.game.main import GraphingMain
+FILENAME= "../amg_emg_force_control2/grapher_game/test_scripts/test_and_ultrasound.txt"
 
 if __name__ == '__main__':
     p_out, p_in = Pipe()
     p1  = SocketPython()
-    p2 = Graphing()
+    p2 = GraphingMain()
 
     # start p2 as another process
-    p2 = Process(target=p2.main, args=(FILENAME, p_out))
+    p2 = Process(target=p2.main, args=(FILENAME, None, p_out))
     # p2.daemon = True
     p2.start()     # Launch the stage2 process
 
