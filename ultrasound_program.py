1bb0dda78424e2468bc2b56475f085e13de02eb3
Chris Mitchell <chrismitchell@berkeley.edu>
2021-05-21 12:01:14 -0700

    added emg sensor args

--- ultrasound_program.py
+++ ultrasound_program.py
@@ -5,7 +5,7 @@ sys.path.append('../')
 
 from amg_emg_force_control import *
 from amg_emg_force_control.grapher_game.game.main import GraphingMain
-FILENAME= "../amg_emg_force_control/grapher_game/test_scripts/test_and_ultrasound.txt"
+FILENAME= "../amg_emg_force_control/grapher_game/test_scripts/ultrasound_emg_trial.txt"
 
 if __name__ == '__main__':
     p_out, p_in = Pipe()
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     p2 = GraphingMain()
 
     # start p2 as another process
-    p2 = Process(target=p2.main, args=(FILENAME, None, p_out))
+    p2 = Process(target=p2.main, args=(FILENAME, ["serial_port=COM8"], p_out))
     # p2.daemon = True
     p2.start()     # Launch the stage2 process
 
