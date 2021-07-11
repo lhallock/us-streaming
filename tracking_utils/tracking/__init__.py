e7a12bb1eea59bac350d80016dae4cddaf108f4c
Bhavna Sud <bhavnasmile1@gmail.com>
2021-07-11 14:23:50 -0700

    cleanup

---
+++ tracking_utils/tracking/__init__.py
@@ -0,0 +1,6 @@
+"""Code to assist with optical flow tracking on ultrasound images.
+
+This package contains modules to apply different filters to ultrasound images to 
+improve optical flow tracking, and set the parameters used in optical flow tracking.
+"""
+__version__ = '0.0.0'
