58e7eb1ecccb8def522ae7efa2fdd201a521e8aa
Bhavna Sud <bhavnasmile1@gmail.com>
2021-07-11 14:32:41 -0700

    adding init.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""Code to recieve ultrasound images and perform optical flow tracking on these images 
+to determine muscle thickness.
+
+This library contains code to recieve ultrasound images from the eZono ultrasound machine,
+perform optical flow tracking on two user selected areas of the muscle to determine 
+muscle thickness, save thickness values and ultrasound images, and start up a grapher to graph
+ultrasound thickness and values from other sensors.
+"""
+__version__ = '0.0.0'
+__author__ = "Bhavna Sud"
